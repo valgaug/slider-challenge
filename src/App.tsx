84f427629a575320d9dd10cbe327e26b32a4cd67
valgaug <valentin.gaugain@hotmail.fr>
2023-11-02 11:00:35 +0000

    firstBranch working

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
 import './styles.css';
 import { Info } from './Info';
+import { Slider } from './Slider';
+import { useState } from 'react';
 
 export default function App() {
+  const [valuePercent, setValuePercent] = useState(50);
+  const [valueProportional, setValueProportional] = useState(0.5);
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Custom Control Slider Challenge</h1>
       <Info />
       <hr />
 
       <p>INTERACTIVE SLIDERS TO GO HERE</p>
+      <Slider label='Percentage Label' max={100} min={0} step={1} value={valuePercent} unit='%' onChange={setValuePercent} />
+      <br />
+      <Slider label='Proportional Label' max={1} min={0} step={0.1} value={valueProportional} unit='%' onChange={setValueProportional} />
     </div>
   );
 }
