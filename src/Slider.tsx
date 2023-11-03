f9301d57fbdd36465469f989b8d14517693afd29
valgaug <valentin.gaugain@hotmail.fr>
2023-11-03 12:08:11 +0000

    wip

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -28,21 +28,27 @@ export function Slider({ label, max, min, step, value, unit, onChange }: Props):
   //   setIsDraggingRef(false);
   // };
 
+  function formatNumber(number: number, step: number): number {
+    const roundedNumber = Math.round(number / step) * step;
+    const decimalPlaces = step.toString().split('.')[1]?.length || 0;
+    return Number(roundedNumber.toFixed(decimalPlaces));
+  }
+
+  const formattedValue = formatNumber(value, step);
   const [isDraggingRef, setIsDraggingRef] = useState(false);
   const [thumbWidth, setThumbWidth] = useState(0);
   const [trackWidth, setTrackWidth] = useState(0);
   const sliderThumbRef = React.createRef<HTMLDivElement>();
   const sliderTrackRef = React.createRef<HTMLDivElement>();
 
-  // const handleMouseDown = (e: React.MouseEvent) => {
-  //   if (sliderTrackRef.current) {
-  //     const rect = sliderTrackRef.current.getBoundingClientRect();
-  //     console.log(e.clientX);
-  //     const newPosition = ((e.clientX - rect.left) / rect.width) * (max - min) + min;
-  //     onChange(newPosition);
-  //     setIsDraggingRef(true);
-  //   }
-  // };
+  const handleMouseDown = (e: React.MouseEvent) => {
+    if (sliderTrackRef.current) {
+      const rect = sliderTrackRef.current.getBoundingClientRect();
+      const newPosition = ((e.clientX - rect.left) / rect.width) * (max - min) + min;
+      onChange(newPosition);
+      setIsDraggingRef(true);
+    }
+  };
 
   useEffect(() => {
     if (sliderThumbRef.current && sliderTrackRef.current) {
@@ -54,24 +60,24 @@ export function Slider({ label, max, min, step, value, unit, onChange }: Props):
   return (
     <div className='slider'>
       <label>{label}</label>
-      <div
-        className='slider-container'
-        ref={sliderTrackRef}
-        // onMouseDown={handleMouseDown}
-        // onMouseMove={handleMouseMove}
-        // onMouseUp={handleMouseUp}
-      >
-        <div className='slider-track'>
+      <div className='slider-container'>
+        <div
+          className='slider-track'
+          ref={sliderTrackRef}
+          onMouseDown={handleMouseDown}
+          // onMouseMove={handleMouseMove}
+          // onMouseUp={handleMouseUp}
+        >
           <div
             className='slider-thumb'
             ref={sliderThumbRef}
             style={{
-              left: `${(1 - thumbWidth / trackWidth) * (((value - min) / (max - min)) * 100)}%`,
+              left: `${(1 - thumbWidth / trackWidth) * (((formattedValue - min) / (max - min)) * 100)}%`,
             }}
           >
             <div className='slider-value'>
               <span>
-                {value}
+                {formattedValue}
                 {unit}
               </span>
             </div>
